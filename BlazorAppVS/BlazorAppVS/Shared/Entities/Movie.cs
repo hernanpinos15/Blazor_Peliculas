ae831a1811b15503901db4b628408a6be9e482f1
HERNAN PINOS <hernan.pinos@outlook.com>
2020-11-14 14:35:51 -0500

    Implementando el formulario para peliculas

--- BlazorAppVS/BlazorAppVS/Shared/Entities/Movie.cs
+++ BlazorAppVS/BlazorAppVS/Shared/Entities/Movie.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace BlazorAppVS.Shared.Entities
 {
     public class Movie
     {
+        public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
-        public DateTime ReleaseDate { get; set; }
+        public string Resumen { get; set; }
+        public bool EnCartelera { get; set; }
+        public string Trailer { get; set; }
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
 
         public string Poster { get; set; }
         public string TitleCortado
