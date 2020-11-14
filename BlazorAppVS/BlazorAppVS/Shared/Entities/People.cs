6e5d1194114aebe9f9a2d80d98155ce69c595341
HERNAN PINOS <hernan.pinos@outlook.com>
2020-11-14 11:00:51 -0500

    Creando un formulario para las personas y colocar en editar y crear

---
+++ BlazorAppVS/BlazorAppVS/Shared/Entities/People.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlazorAppVS.Shared.Entities
+{
+    public class People
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        public string Biografia { get; set; }
+        public string Foto { get; set; }
+        [Required]
+        public DateTime? FechaNacimiento { get; set; }
+    }
+}
